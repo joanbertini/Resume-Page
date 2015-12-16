b2246e6795e1e0f053e07d786dd80616060d7558
PixelNew <joanbertini@gmail.com>
2015-12-16 19:28:20 +0100

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,11 +1,8 @@
-This is empty on purpose! Your code to build the resume will go here.
- */
- 
+/* This is empty on purpose! Your code to build the resume will go here. */
+
+var name = "Stéphanie Malafosse"; 
 var formattedName = HTMLheaderName.replace("%data%", name);
-var name = "Stéphanie Malafosse";
-var formattedName = HTMLheaderName.replace("%data%", "Stéphanie Malafosse");
 $("#header").prepend(formattedName);
-var formattedRole = HTMLheaderRole.replace("%data%", role);
 var role = "Web designer & Développeuse web";
-var formattedRole = HTMLheaderRole.replace("%data%", "Web designer & Développeuse web");
+var formattedRole = HTMLheaderRole.replace("%data%", role);
 $("#header").append(formattedRole);
