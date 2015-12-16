c48e3f10facdaf4a30b4c001aff8542d34ee7ceb
PixelNew <s.malafosse@gmail.com>
2015-12-16 19:20:12 +0100

    Code more precise.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,7 +2,9 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
  
-var formattedName = HTMLheaderName.replace("%data%", "Stéphanie Malafosse");
+var formattedName = HTMLheaderName.replace("%data%", name);
+var name = "Stéphanie Malafosse";
 $("#header").prepend(formattedName);
-var formattedRole = HTMLheaderRole.replace("%data%", "Web designer & Développeuse web");
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+var role = "Web designer & Développeuse web";
 $("#header").append(formattedRole);
