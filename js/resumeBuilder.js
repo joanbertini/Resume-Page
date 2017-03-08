8498c45eefc57a73f8e5e8c49e37e62312bb25d6
PixelNew <joanbertini@gmail.com>
2017-03-08 15:35:20 +0100

    Add files via upload

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,8 +1,3 @@
-/* This is empty on purpose! Your code to build the resume will go here. */
-
-var name = "Stéphanie Malafosse"; 
-var formattedName = HTMLheaderName.replace("%data%", name);
-$("#header").prepend(formattedName);
-var role = "Web designer & Développeuse web";
-var formattedRole = HTMLheaderRole.replace("%data%", role);
-$("#header").append(formattedRole);
+/*
+This is empty on purpose! Your code to build the resume will go here.
+ */
